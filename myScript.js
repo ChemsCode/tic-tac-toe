0ad6c48cff4c20afc202d50d1f2052d1c7f8ddab
ChemsCode <saidiceb@gmail.com>
2022-03-11 16:12:12 -0500

    implemented the logic that doesn't let a player play on top of an allready occupied case

--- myScript.js
+++ myScript.js
@@ -1,6 +1,7 @@
 //making the grid
 let boardDisplay = document.getElementById("board")
 let displayBoard = [];
+let playCounter = 0;
 
 function makeGrid(){
     
@@ -15,15 +16,24 @@ function makeGrid(){
 
 makeGrid();
 
-function assignEventListener(sign){
+// function assignEventListener(sign, boardArray){
+//     for(let i = 0 ; i < 9; i++){
+//         document.getElementById(`${i}`).addEventListener("click", () => {
+//             document.getElementById(`${i}`).innerHTML = sign; 
+//             boardArray[i] = sign;
+//             console.log(boardArray);
+//         });
+//     }
+// }
+
+function playeGameTurn(){
     for(let i = 0 ; i < 9; i++){
         document.getElementById(`${i}`).addEventListener("click", () => {
-            document.getElementById(`${i}`).innerHTML = sign; 
+            playGame.playTurn(player1, player2, (document.getElementById(`${i}`).getAttribute('id')));
+            console.log("The documnet ID value returned by playedGameTurn is " + (document.getElementById(`${i}`).getAttribute('id')));
         });
     }
 }
-
-
 
 //making the gameboard module
 
@@ -38,12 +48,20 @@ const gameBoard = (() => {
         }
     }
 
-    const updateBoard = (player, positionChosen) => {
+    const updateBoard = (sign, positionChosen) => {
+        
         if(board[positionChosen] == ''){
-        board[positionChosen] = player.getSign();
-        printBoard();
-        document.getElementById(`${positionChosen}`).innerHTML = board[positionChosen];
+            console.log("The positionChosen's value is: " + board[positionChosen] + " and the index of that poisition is " + positionChosen)
+            document.getElementById(`${positionChosen}`).innerHTML = sign; 
+            board[positionChosen] = sign;
         }
+        // else{
+        //     playeGameTurn();
+        // }
+
+        //board[positionChosen] = sign
+        // assignEventListener(sign, board);
+        
     }
 
     const printBoard = () => {
@@ -53,7 +71,7 @@ const gameBoard = (() => {
     const getBoardElement = (index) => board[index];
 
     return {
-        clearBoard, updateBoard, printBoard, getBoardElement
+        clearBoard, updateBoard, printBoard, getBoardElement, board
     };
 })();
 
@@ -62,23 +80,29 @@ const gameBoard = (() => {
 
 const playGame = (() => {
 
-    const playTurn = (player1, player2) =>{
+    const playTurn = (player1, player2, boardPosition) =>{
         let sign = checkTurn(player1, player2);
-        console.log(sign);
-        assignEventListener(sign);
+        console.log("This is the sign after the checkTurn method: " + sign);
+        gameBoard.updateBoard(sign, boardPosition);
+        // assignEventListener(sign, gameBoard.board);
+        // playeGameTurn();
 
     }
 
     const checkTurn = (player1, player2) =>{
-        let p1_counter = 0;
-        let p2_counter = 0;
 
-        for(i = 0; i < 9; i++){
-            if(gameBoard.getBoardElement(i) == player1.getSign())
+        let p1_counter = 0;
+        let p2_counter = 0;    
+
+        for(let i = 0; i < 9; i++){
+            if(gameBoard.board[i] == player1.getSign())
             p1_counter++;
-            else if(gameBoard.getBoardElement(i) == player2.getSign())
+            else if(gameBoard.board[i] == player2.getSign())
             p2_counter++;
         }
+
+        console.log("This is the count of X's: " + p1_counter);
+        console.log("This is the count of O's: " + p2_counter);
 
         if((p1_counter == p2_counter))
         return player1.getSign()
@@ -111,7 +135,9 @@ const player2 = Player("Eddine", "O")
 
 // playGame.checkTurn(player1, player2);
 
-// for(i = 0; i < 9; i++){
-// playGame.playTurn(player1, player2);
-// }
+
+
+// assignEventListener('X', gameBoard.board);
+
+playeGameTurn();
 
